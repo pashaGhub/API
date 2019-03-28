cc3be938c75a9e8e15a2c90e8558363437538849
pashaGhub <pawelsucharew@gmail.com>
2019-03-28 21:23:30 +0200

    03/28

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,5 +2,17 @@ $(function() {
   $('#burger').on('click', function() {
     $('#side-menu').toggleClass('side-menu--closed');
     $('body').toggleClass('sidenav--closed');
+
   })
+
+  let basename = $('#side-menu').find('li.active span').text();
+  $('#basename')
+  .text(basename)
+  .attr('href', `/${basename.toLowerCase()}.html`);
+
+  $('#activities').on('click', function() {
+    $('#activities-menu').toggleClass('activities-menu--active');
+  })
+
+
 })
